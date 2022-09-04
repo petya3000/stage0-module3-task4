612605b1f68c13da0067d19c8a42674864112802
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-04 17:09:43 +0300

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.print((char)99);
+            System.out.print((char)104);
+            System.out.print((char)97);
+            System.out.print((char)114);
     }
 }

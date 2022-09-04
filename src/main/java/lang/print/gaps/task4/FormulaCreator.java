678880b84202f1b5bea8b6c9022f063c7bb11103
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-04 17:12:56 +0300

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a, b;
+        int a = 1, b = 2;
         (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
